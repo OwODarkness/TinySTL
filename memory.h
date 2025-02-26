6e667cfa2b1d01fd5c638aa494fdc16875d31679
owodarkness <owodarkness@163.com>
2025-02-26 22:46:34 +0800

    add deleter

--- memory.h
+++ memory.h
@@ -4,9 +4,27 @@
 #include <utility>
 
 namespace tinystl{
+    template<typename T>
+    struct default_delete{
+        default_delete() = default;
+        void operator()(T* p)
+        {
+            std::cout << "default delete\n" ;
+            delete p;
+        }
+    };
+
+    template<typename T>
+    struct default_delete<T[]>{
+        void operator()(T* p)
+        {
+            delete[] p;
+        }
+    };
+
 
     //shared_ptr
-    template<typename T>
+    template<typename T,typename Deleter = default_delete<T>>
     class shared_ptr{
     public:
         using value_type = T;
@@ -15,16 +33,20 @@ namespace tinystl{
 
         inline size_t use_count() const noexcept{return ref_count_ != nullptr ? *ref_count_ : 0;}
 
-        inline pointer get() noexcept{return data_;}
+        inline pointer get() const noexcept{return data_;}
+
+        inline bool unique() const noexcept{return use_count() == 1;}
+
+        void reset(pointer p);
 
         template<typename U, typename...VARS> 
         friend shared_ptr<U> make_shared(VARS... vars);
 
-        shared_ptr(const pointer p);
+        explicit shared_ptr(const pointer p);
 
-        shared_ptr(const shared_ptr<T>& p);
+        shared_ptr(const shared_ptr& p);
 
-        shared_ptr(shared_ptr<T>&& p);
+        shared_ptr(shared_ptr&& p);
 
         shared_ptr& operator=(const shared_ptr& p);
 
@@ -34,6 +56,8 @@ namespace tinystl{
         
         pointer operator->() const{return data_;}
 
+        operator bool() const{return (data_ != nullptr) ;}
+
         template<typename U>
         friend std::ostream& operator<<(std::ostream& out, const shared_ptr<U>& p);
 
@@ -41,6 +65,8 @@ namespace tinystl{
     private:
         pointer data_;
         size_t* ref_count_;
+
+        Deleter deleter;
     };
 
     template<typename U, typename... VARS>
@@ -53,18 +79,16 @@ namespace tinystl{
     }
 
 
-    template<typename T>
-    shared_ptr<T>::shared_ptr():data_(nullptr), ref_count_(nullptr){}
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>::shared_ptr():
+    data_(nullptr), ref_count_(nullptr), deleter(Deleter()){}
 
-    template<typename T>
-    shared_ptr<T>::shared_ptr(typename shared_ptr<T>::pointer p):
-    data_(p), ref_count_(new size_t(1))
-    {
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>::shared_ptr(typename shared_ptr<T, Deleter>::pointer p):
+    data_(p), ref_count_(new size_t(1)), deleter(Deleter()){}
 
-    }
-
-    template<typename T>
-    shared_ptr<T>::shared_ptr(const shared_ptr<T>& p)
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>::shared_ptr(const shared_ptr<T, Deleter>& p)
     {
         this->data_ = p.data_;
         this->ref_count_ = p.ref_count_;
@@ -72,8 +96,8 @@ namespace tinystl{
             (*this->ref_count_)++;
     }
 
-    template<typename T>
-    shared_ptr<T>::shared_ptr(shared_ptr<T>&& p)
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>::shared_ptr(shared_ptr&& p)
     {
         this->data_ = p.data_;
         this->ref_count_ = p.ref_count_;
@@ -81,8 +105,8 @@ namespace tinystl{
         p.ref_count_ = nullptr;
     }
 
-    template<typename T>
-    shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<T>& p)
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>& shared_ptr<T, Deleter>::operator=(const shared_ptr& p)
     {
         if(p.ref_count_ != nullptr)
         {
@@ -91,20 +115,22 @@ namespace tinystl{
         }
         if(ref_count_ !=nullptr && --(*ref_count_) == 0)
         {
-            delete data_;
+            deleter(data_);
             delete ref_count_;
+            data_  = nullptr;
+            ref_count_ = nullptr;
         }
         this->data_ = p.data_;
         this->ref_count_ = p.ref_count_;
         return *this;
     }
 
-    template<typename T>
-    shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr<T>&& p)
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>& shared_ptr<T, Deleter>::operator=(shared_ptr&& p)
     {
         if(ref_count_ != nullptr && --(*ref_count_) == 0)
         {
-            delete data_;
+            deleter(data_);
             delete ref_count_;
         }
         this->data_ = p.data_;
@@ -121,13 +147,33 @@ namespace tinystl{
         return out;
     }
 
-    template<typename T>
-    shared_ptr<T>::~shared_ptr()
+    template<typename T, typename Deleter>
+    shared_ptr<T, Deleter>::~shared_ptr()
     {
         if(ref_count_ != nullptr && --(*ref_count_) == 0)
         {
-            delete data_;
+            //delete data_;
+            deleter(data_);
             delete ref_count_;
+            data_ = nullptr;
+            ref_count_ = nullptr;
+        }
+    }
+
+    template<typename T, typename Deleter>
+    void shared_ptr<T, Deleter>::reset(typename shared_ptr<T, Deleter>::pointer p)
+    {
+        if(ref_count_ != nullptr && --(*ref_count_)==0)
+        {
+            deleter(data_);
+            delete ref_count_;
+            data_ = nullptr;
+            ref_count_ = nullptr;
+        }
+        if(p != nullptr)
+        {
+            data_ = p;
+            ref_count_ = new size_t(1);
         }
     }
 }
--- test.cpp
+++ test.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, char** argv){
     tinystl::shared_ptr<int> p(new int(3));
-    std::cout << p << "\n";
+
     return 0;
 }
