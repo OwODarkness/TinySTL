1c9cc6f7ca779d9979947735327feca5a45f837c
owodarkness <owodarkness@163.com>
2025-02-28 09:01:01 +0800

    update unique_ptr

--- memory.h
+++ memory.h
@@ -9,7 +9,6 @@ namespace tinystl{
         default_delete() = default;
         void operator()(T* p)
         {
-            std::cout << "default delete\n" ;
             delete p;
         }
     };
@@ -31,22 +30,21 @@ namespace tinystl{
         using pointer = T*;
         explicit shared_ptr();
 
-        inline size_t use_count() const noexcept{return ref_count_ != nullptr ? *ref_count_ : 0;}
-
-        inline pointer get() const noexcept{return data_;}
-
-        inline bool unique() const noexcept{return use_count() == 1;}
-
-        void reset(pointer p);
-
-        template<typename U, typename...VARS> 
-        friend shared_ptr<U> make_shared(VARS... vars);
-
         explicit shared_ptr(const pointer p);
 
         shared_ptr(const shared_ptr& p);
 
         shared_ptr(shared_ptr&& p);
+
+        inline size_t use_count() const noexcept{return ref_count_ != nullptr ? *ref_count_ : 0;}
+
+        inline pointer get() const noexcept{return data_;}
+
+        inline Deleter& get_deleter() const noexcept {return deleter_;}
+
+        inline bool unique() const noexcept{return use_count() == 1;}
+
+        void reset(pointer p);
 
         shared_ptr& operator=(const shared_ptr& p);
 
@@ -62,11 +60,13 @@ namespace tinystl{
         friend std::ostream& operator<<(std::ostream& out, const shared_ptr<U>& p);
 
         ~shared_ptr();
+        
+        template<typename U, typename...VARS> 
+        friend shared_ptr<U> make_shared(VARS... );
     private:
         pointer data_;
         size_t* ref_count_;
-
-        Deleter deleter;
+        Deleter deleter_;
     };
 
     template<typename U, typename... VARS>
@@ -78,15 +78,17 @@ namespace tinystl{
         return p;
     }
 
-
+    //default constructor
     template<typename T, typename Deleter>
     shared_ptr<T, Deleter>::shared_ptr():
-    data_(nullptr), ref_count_(nullptr), deleter(Deleter()){}
+    data_(nullptr), ref_count_(nullptr), deleter_(Deleter()){}
 
+    //constructor with parameter raw pointer
     template<typename T, typename Deleter>
-    shared_ptr<T, Deleter>::shared_ptr(typename shared_ptr<T, Deleter>::pointer p):
-    data_(p), ref_count_(new size_t(1)), deleter(Deleter()){}
+    shared_ptr<T, Deleter>::shared_ptr(const typename shared_ptr<T, Deleter>::pointer p):
+    data_(p), ref_count_(new size_t(1)), deleter_(Deleter()){}
 
+    //copy constructor
     template<typename T, typename Deleter>
     shared_ptr<T, Deleter>::shared_ptr(const shared_ptr<T, Deleter>& p)
     {
@@ -96,6 +98,7 @@ namespace tinystl{
             (*this->ref_count_)++;
     }
 
+    //move constructor
     template<typename T, typename Deleter>
     shared_ptr<T, Deleter>::shared_ptr(shared_ptr&& p)
     {
@@ -115,7 +118,7 @@ namespace tinystl{
         }
         if(ref_count_ !=nullptr && --(*ref_count_) == 0)
         {
-            deleter(data_);
+            deleter_(data_);
             delete ref_count_;
             data_  = nullptr;
             ref_count_ = nullptr;
@@ -130,7 +133,7 @@ namespace tinystl{
     {
         if(ref_count_ != nullptr && --(*ref_count_) == 0)
         {
-            deleter(data_);
+            deleter_(data_);
             delete ref_count_;
         }
         this->data_ = p.data_;
@@ -153,7 +156,7 @@ namespace tinystl{
         if(ref_count_ != nullptr && --(*ref_count_) == 0)
         {
             //delete data_;
-            deleter(data_);
+            deleter_(data_);
             delete ref_count_;
             data_ = nullptr;
             ref_count_ = nullptr;
@@ -165,7 +168,7 @@ namespace tinystl{
     {
         if(ref_count_ != nullptr && --(*ref_count_)==0)
         {
-            deleter(data_);
+            deleter_(data_);
             delete ref_count_;
             data_ = nullptr;
             ref_count_ = nullptr;
@@ -175,6 +178,76 @@ namespace tinystl{
             data_ = p;
             ref_count_ = new size_t(1);
         }
+    }
+
+    template<typename T, typename Deleter = default_delete<T>>
+    class unique_ptr{
+    public:
+        using value_type = T;
+        using pointer = T*;
+        
+        explicit unique_ptr();
+        explicit unique_ptr(const pointer p);
+
+        //unique_ptr can no be copied
+        unique_ptr(const unique_ptr& p) = delete;
+        unique_ptr(unique_ptr&& p);
+        
+        unique_ptr& operator=(const unique_ptr& p) = delete;
+        unique_ptr& operator=(unique_ptr&& p);
+
+        inline pointer get() const{return data_;}
+        inline Deleter& get_deleter() const noexcept {return deleter_;}
+
+        value_type& operator*(){return *data_;}
+        pointer operator->() const{return data_;}
+        operator bool() const{return data_ != nullptr;}
+
+        ~unique_ptr();
+
+        template<typename U, typename... VARS>
+        friend unique_ptr<U> make_unique(VARS... );
+        
+    private:
+        pointer data_;
+        Deleter deleter_;
+    };
+
+    template<typename T, typename... VARS>
+    unique_ptr<T> make_unique(VARS... vars)
+    {
+        unique_ptr<T> p;
+        p.data_ = new T(vars...);
+        return p;
+    }
+
+    template<typename T, typename Deleter>
+    unique_ptr<T, Deleter>::unique_ptr():data_(nullptr), deleter_(Deleter()){}
+
+    template<typename T, typename Deleter>
+    unique_ptr<T, Deleter>::unique_ptr(const typename unique_ptr<T, Deleter>::pointer p):
+    data_(p), deleter_(Deleter()){}
+
+    template<typename T, typename Deleter>
+    unique_ptr<T, Deleter>::unique_ptr::unique_ptr(unique_ptr&& p )
+    {
+        this->data_ = p.data_;
+        p.data_  = nullptr;
+    }
+
+    template<typename T, typename Deleter>
+    unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(unique_ptr&& p)
+    {
+        deleter_(data_);
+        this->data_ = p.data_;
+        p.data_ = nullptr;
+        return *this;
+    }
+
+    template<typename T, typename Deleter>
+    unique_ptr<T, Deleter>::unique_ptr::~unique_ptr()
+    {
+        deleter_(data_);
     }
 }
 
--- test.cpp
+++ test.cpp
@@ -3,7 +3,6 @@
 #include "memory.h"
 
 int main(int argc, char** argv){
-    tinystl::shared_ptr<int> p(new int(3));
-
+    
     return 0;
 }
